fe9739494f275a8228fc2bda1ef1e1ddcd231a9b
Abraham Fatoki <fatokiabraham@gmail.com>
2022-07-04 12:29:14 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints the alphabet in lowercase,
+ * main - prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
@@ -11,9 +11,9 @@ int main(void)
 	char ch;
 
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
-		putchar(ch);							
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
