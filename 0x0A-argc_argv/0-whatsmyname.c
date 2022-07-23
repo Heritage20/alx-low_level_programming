796d9b5c9e320038ced341ed93436653cd2917e6
Abraham Fatoki <fatokiabraham@gmail.com>
2022-07-22 20:00:34 -0700

    fix on whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,18 @@
 
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - program that prints its name, followed by a new line
  *
- * Return: Always 0 (Success)
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
