697a109716f1706e107c5a51ccdc0ade7082c32d
Abraham Fatoki <fatokiabraham@gmail.com>
2022-08-01 12:07:21 +0100

    Fix on each tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * _strlen - returns the length of a string
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * free_dog - frees memory allocated for a struct dog
